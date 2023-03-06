49c1024c6af89b62e32a978c9311cac22e75ee6e
Muhammad Zohair Baig <112842005+Muhammad-Zohair-Baig@users.noreply.github.com>
2023-03-06 10:18:06 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,9 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_cors import CORS
-import json
-import matplotlib.pyplot as plt
+from flask import Flask, request,jsonify
 from PIL import Image
-import io
 import numpy as np
 import mediapipe as mp
-import cv2
 import math
-from mediapipe.framework.formats import landmark_pb2
-
+from flask_cors import CORS
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
@@ -20,8 +13,6 @@ face_mesh = mp_face_mesh.FaceMesh(
         min_tracking_confidence=0.5)
 app = Flask(__name__)
 CORS(app)
-api = Api(app)
-
 @app.route('/measurement', methods=['POST'])
 def measurement():
     print('HI')
@@ -34,16 +25,21 @@ def measurement():
     max_nl = 0
     max_ne = 0
     img_dist = 0.001
+    print("hi")
     for i in x.listvalues():
+        print("hi")
         c = Image.open(i[0])
+        print("hi1")
         image = np.array(c)
+        print("hi2")
         sh = image.shape
+        print("hi3")
         h = sh[0]
         w = sh[1]
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
         results = face_mesh.process(image)
-
+        print("hi")
         # Draw the face mesh annotations on the image.
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks :
@@ -58,7 +54,8 @@ def measurement():
             image_dist_6 = mp_drawing.math.dist((l[168].x*int(h),l[168].y*int(w)),(l[4].x*int(h),l[4].y*int(w)))
             image_dist_7 = mp_drawing.math.dist((l[102].x*int(h),l[102].y*int(w)),(l[4].x*int(h),l[4].y*int(w)))
             real_dist = 1.1
-            if image_dist >img_dist:
+            print("HIIIIII")
+            if img_dist<image_dist:
                 img_dist=image_dist
             otpr = real_dist/img_dist
             i = otpr*image_dist_2
@@ -79,8 +76,8 @@ def measurement():
             ne = otpr*image_dist_7
             if max_ne<ne:
                 max_ne=ne
+            print("hi")
 
-        
     print(max_eye,max_fw,
     max_pd,
     max_nw,
@@ -91,10 +88,12 @@ def measurement():
     angle = math.degrees(angle)+3
     print(angle)
     print("////////////////////////////")
-    return({"max_eye":max_eye,"max_fw":max_fw,
+    response = jsonify(message={"max_eye":max_eye,"max_fw":max_fw,
     "max_pd":max_pd,
     "max_nw":max_nw,
     "angle":angle})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return(response)
 
 
 
